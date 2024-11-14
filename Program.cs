c2a2c6a64e547e07f4951faa1da9216083e6e6b7
Alexkcq <qalexander785@gmail.com>
2024-11-13 23:18:01 -0500

    Primer commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using EJERCICIO_TRABAJO_DE_PROG.___GRUPO_5;
+string opcion;
+List<Desayuno> desayunos = new List<Desayuno>();
+
+void MostrarMenu()
+{
+    Console.Clear();
+    Console.WriteLine("Menú de Desayunos:\n");
+    Console.WriteLine("a) Crear desayuno\n");
+    Console.WriteLine("b) Listar desayunos\n");
+    Console.WriteLine("c) Eliminar desayuno\n");
+    Console.WriteLine("d) Salir\n");
+    Console.Write("Selecciona una opción: \n");
+}
